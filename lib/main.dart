0f800f063c502af2e2d907f8af458617a7ae782b
alfatra <94300215+alfatra@users.noreply.github.com>
2025-09-17 11:40:38 +0800

    update video

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: SplashScreen(), // HALAMAN AWAL
+      home: const SplashScreen(), // HALAMAN AWAL
     );
   }
 }
